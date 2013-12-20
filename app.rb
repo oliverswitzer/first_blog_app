e95bbfe24aa406d7f3171b6d70b2937388ad2b9b
Oliver Switzer <oliverswitzer@gmail.com>
2013-12-20 12:51:22 -0500

    create layout.erb and route for a new post

--- app.rb
+++ app.rb
@@ -5,5 +5,31 @@ set :database, "sqlite3:///blog.db"
 
 class Post < ActiveRecord::Base
 
-end
+  get "/" do
+    @posts = Post.order("created_at DESC")
+    erb :"posts/index"
+  end
+
+  get "/posts/new" do
+    @title = "New Post"
+    @post = Post.new
+    erb :"posts/new"
+  end
+
+
+  helpers do 
+    def title
+      if @title 
+        "#{@title} -- My blog"
+      else
+        "My Blog"
+      end
+    end
+
+    def pretty_date(time)
+      time.strftime("%d %b %Y")
+    end
+  end
+
+end 
 
